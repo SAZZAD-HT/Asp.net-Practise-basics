3b779e7ec6f72039dd6523a8703f8030d5b38d11
SAZZAD-HT <sazzadaiub1@gmail.com>
2022-10-09 23:15:38 +0600

    .

---
+++ validation/validation/validation/Controllers/StudentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using validation.Models;
+using Validation = validation.Models.Validation;
+
+namespace validation.Controllers
+{
+    public class StudentController : Controller
+    {
+        // GET: Student
+      
+        public ActionResult Index()
+        {
+            ViewBag.Subscription = "Normal Subscription";
+
+            ViewBag.price = new String[] { "10.00", "2.00", "3.00", "4.00" };
+
+            return View();
+        }
+        [HttpGet]
+        public ActionResult CreateAccount()
+        {
+            return View(); 
+        }
+        [HttpPost]
+        public ActionResult CreateAccount(Validation valid )
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            return View(valid);
+        }
+    }
+}
---
+++ validation/validation/validation/Models/Foot.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace validation.Models
+{
+    public class Foot
+    {
+        public string subcription { get; set; }
+        public string sub1 { get; set; }
+        public string price { get; set; }
+    }
+}
